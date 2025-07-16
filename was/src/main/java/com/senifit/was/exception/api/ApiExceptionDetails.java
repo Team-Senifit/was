e2d78167597d6f7e2e8dd12cf921a9105208bf71
tmin002 <ssh9930@gmail.com>
2025-07-16 21:40:26 +0900

    MLFM-15 feat: apiresponse의 data 필드 null일때도 {} 반환하도록 설정

--- was/src/main/java/com/senifit/was/exception/api/ApiExceptionDetails.java
+++ was/src/main/java/com/senifit/was/exception/api/ApiExceptionDetails.java
@@ -7,20 +7,23 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ApiExceptionDetails {
 
-    UNHANDLED(500, "INTERNAL000", "내부 오류가 발생했습니다. 잠시 후 다시 시도해 주세요."),
-    BAD_REQUEST(400, "BADREQUEST000", "잘못된 요청입니다."),
-    ACCESS_DENIED(403, "ACCESS000", "권한이 부족합니다."),
-    NOT_FOUND(404, "NOTFOUND000", "잘못된 경로입니다."),
+    UNHANDLED(500, "INTERNAL:000", "내부 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.", true),
+    BAD_REQUEST(400, "BAD_REQUEST:000", "잘못된 요청입니다.", true),
+    ACCESS_DENIED(403, "ACCESS:000", "권한이 부족합니다.", true),
+    NOT_FOUND(404, "NOTFOUND:000", "잘못된 경로입니다.", true),
 
     // Sign Up
-    SIGNUP_VALIDATION_ID_EXISTS(409, "SIGNUP_VAL:001", "이미 존재하는 센터 ID 입니다."),
+    SIGNUP_VALIDATION_ID_EXISTS(409, "SIGNUP_VAL:001", "이미 존재하는 센터 ID 입니다.", true),
 
     // Sign In
-    SIGNIN_ELSE(403, "SIGNIN:000", "인증 과정 중 오류가 발생했습니다. 잠시후 다시 시도해 주세요."),
-    SIGNIN_AUTHENTICATION_FAIL(401, "SIGNIN:001", "아이디 / 비밀번호를 확인하세요."),
-    SIGNIN_MAXIMUM_SESSION_COUNT(403, "SIGNIN:002", "동시에 접속할 수 있는 최대 한도를 초과했습니다.");
+    SIGNIN_ELSE(403, "SIGNIN:000", "인증 과정 중 오류가 발생했습니다. 잠시후 다시 시도해 주세요.", true),
+    SIGNIN_AUTHENTICATION_FAIL(401, "SIGNIN:001", "아이디 / 비밀번호를 확인하세요.", true),
+    SIGNIN_MAXIMUM_SESSION_COUNT(403, "SIGNIN:002", "동시에 접속할 수 있는 최대 한도를 초과했습니다.", true);
 
     private final int httpStatusCode;
     private final String errorCode;
     private final String message;
+
+    private final boolean enableLogging;
+
 }
