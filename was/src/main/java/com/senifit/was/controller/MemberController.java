a26a857e4f8552e901cc3efa984a3de065fbd592
Park-JuH <parkjuh2021@gmail.com>
2025-07-20 17:59:39 +0900

    MLFM-26 CI/CD 테스트

--- was/src/main/java/com/senifit/was/controller/MemberController.java
+++ was/src/main/java/com/senifit/was/controller/MemberController.java
@@ -2,6 +2,6 @@ package com.senifit.was.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("users")
-public class UserController {
+@RestController("members")
+public class MemberController {
 }
