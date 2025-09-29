7acf2d4fe70f03bff5b72b2c34d64e3e89d84434
Park-JuH <parkjuh2021@gmail.com>
2025-09-29 21:41:54 +0900

    MLFM-134 Security 설정 수정

--- was/src/main/java/com/senifit/was/config/SecurityConfig.java
+++ was/src/main/java/com/senifit/was/config/SecurityConfig.java
@@ -52,8 +52,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                    .requestMatchers("/actuator/health", "/actuator/prometheus", "/actuator/metrics/**").permitAll()
-                    .anyRequest().authenticated()
+                        .requestMatchers("/actuator/health", "/actuator/prometheus", "/actuator/metrics/**").permitAll()
                 )
                 .userDetailsService(userDetailsService)
                 .formLogin(form -> form
