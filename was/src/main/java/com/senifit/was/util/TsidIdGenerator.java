6626352a45fdd1152e3f30b47a46c2bac1babe82
tmin002 <ssh9930@gmail.com>
2025-11-17 12:01:57 +0900

    feat: TSID 기반 ID 생성기 추가
    - TsidIdGenerator 유틸리티 클래스 추가
    - Program 엔티티에서 TSID 사용을 위한 기반 코드

--- was/src/main/java/com/senifit/was/util/TsidIdGenerator.java
+++ was/src/main/java/com/senifit/was/util/TsidIdGenerator.java
@@ -5,7 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TsidIdGenerator {
+
+    // 2^53 - 1 (JavaScript Number.MAX_SAFE_INTEGER)
+    // 프론트엔드에서 64bit 범위까지 고려하지 않은 것으로 확인되어
+    // 기본 타입인 Number의 최대 값을 사용합니다.
+    // 물론 충돌 가능성은 극히 낮아 고려할 필요는 없으나 기존 TSID 방식보다 늘어난건 사실.
+    //
+    // TODO: 이 문제 해결 필요.
+    private static final long MAX_SAFE_INTEGER = 9007199254740991L; 
+
     public Long nextId() {
-        return TsidCreator.getTsid().toLong();
+        long tsidValue = TsidCreator.getTsid().toLong();
+        return Math.abs(tsidValue % MAX_SAFE_INTEGER);
     }
 }
